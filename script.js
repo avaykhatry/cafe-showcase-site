abe3a73e5322ee2580f2d6823dce65b5b3e99b09
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-08-17 21:38:02 +0545

    Add menu items and a category-based navigation for them

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/products.json")
+    .then((response) => response.json())
+    .then((product) => {
+      const items = product.menu;
+
+      for (let i = 0; i < items.length; i++) {
+        // category li
+        const categoryLi = document.createElement("li");
+        categoryLi.id = `category-${i}`;
+        categoryLi.textContent = items[i].category;
+
+        // nav link
+        const categoryNavLink = document.createElement("a");
+        categoryNavLink.href = `#category-${i}`;
+        categoryNavLink.textContent = items[i].category;
+
+        document.querySelector("#category-nav").appendChild(categoryNavLink);
+        
+        // ul for food items
+        const foodUl = document.createElement("ul");
+        foodUl.classList.add("item-list");
+
+        for (let j = 0; j < items[i].items.length; j++) {
+          const foodLi = document.createElement("li");
+          foodLi.textContent = items[i].items[j].name;
+          foodUl.appendChild(foodLi);
+        }
+
+        // food ul attached to category li
+        categoryLi.appendChild(foodUl);
+
+        document.querySelector("#menu-list").appendChild(categoryLi);
+      }
+    })
+    .catch((error) => console.error("Error loading JSON:", error));
+});
